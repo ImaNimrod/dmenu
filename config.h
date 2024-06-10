799db72f033ae770e3e650a69c1481c91f7e2764
nimrod <nun.ya@business.com>
2024-06-10 11:42:07 -0400

    color change

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const char *prompt = NULL;           /* -p  option; prompt to the left of
 
 static const char *colors[SchemeLast][2] = {
 	/*               fg         bg        */
-	[SchemeNorm] = { "#cecece", "#222222" },
+	[SchemeNorm] = { "#dedede", "#222222" },
 	[SchemeSel]  = { "#ffffff", "#6f00be" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
