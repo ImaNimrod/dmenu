cdf0ad8e317ff137032389bdea7ad7db3ab3a0b6
nimrod <nun.ya@business.com>
2026-02-17 23:19:29 -0500

    update my personal config

--- config.def.h
+++ config.def.h
@@ -8,14 +8,14 @@ static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the o
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"mononoki Nerd Font:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
-static char selbgcolor[]  = "#005577";
+static char selbgcolor[]  = "#6f00be";
 static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { normfgcolor, normbgcolor },
@@ -32,7 +32,7 @@ static unsigned int lines      = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 5;
 
 /*
  * Xresources preferences to load at startup
@@ -41,5 +41,5 @@ ResourcePref resources[] = {
 	{ "normfgcolor", STRING, &normfgcolor },
 	{ "normbgcolor", STRING, &normbgcolor },
 	{ "selfgcolor",  STRING, &selfgcolor },
-	{ "selbgcolor",  STRING, &selbgcolor },
+	{ "color2",  STRING, &selbgcolor },
 };
